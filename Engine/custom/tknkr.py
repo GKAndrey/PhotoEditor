25d6ff6246b2b89f8fcc7451a21aa6ffbbccb550
GKAndrey <druzgaandrey@gmail.com>
2023-05-24 16:28:15 +0300

    Creating a window and icon

--- Engine/custom/tknkr.py
+++ Engine/custom/tknkr.py
@@ -1,3 +1,18 @@
 import tkinter
 import customtkinter
+import os
 
+PATH = os.path.abspath(__file__ + '/..')
+
+menu = tkinter.Tk()
+menu.geometry("1600x900")
+customtkinter.set_appearance_mode("System")
+menu.title("Photoshopingist")
+menu.iconbitmap(os.path.join(PATH,"phot_icon.ico"))
+menu["bg"] = "gray22"
+
+
+
+
+
+menu.mainloop()
