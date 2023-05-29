be7fdaa7fc5c833aaf06a017058d8956201d47fa
GKAndrey <druzgaandrey@gmail.com>
2023-05-29 19:03:25 +0300

    Customizing Design and Filters Co-authored-by: Zeith22 <zeith2mail@gmail.com>

--- Engine/custom/tknkr.py
+++ Engine/custom/tknkr.py
@@ -8,9 +8,9 @@ menu.geometry("1280x720")
 customtkinter.set_appearance_mode("System")
 menu.title("Photoshopingist")
 menu.iconbitmap(os.path.join(PATH,"custom","phot_icon.ico"))
-menu["bg"] = "gray22"
+menu["bg"] = "lightskyblue4"
 
-# Меню
+# Меню  
 file_menu = tkinter.Menu(tearoff=0)
 file_menu.add_command(label="Local File", command=local_select_img)
 file_menu.add_command(label="URL FIle", command=web_select_img)
@@ -19,25 +19,33 @@ main_menu = tkinter.Menu()
 main_menu.add_cascade(label="File", menu = file_menu)
 menu.config(menu=main_menu)
 
-# label1 = ctk.CTkLabel(master=menu,
-#                       text=f"Назва світлини: {return_info[0]["Назва світлини"]}")
-# label2 = ctk.CTkLabel(master=menu,
-#                       text=f"Вага світлини: {return_info[0]["Вага світлини"]}")
-# label3 = ctk.CTkLabel(master=menu,
-#                       text=f"Висота світлини: {return_info[0]["Висота світлини"][0]}")
-# label4 = ctk.CTkLabel(master=menu,
-#                       text=f"Ширина світлини: {return_info[0]["Ширина світлини"][0]}")
-# label5 = ctk.CTkLabel(master=menu,
-#                       text=f"Формат світлини: {return_info[0]["Формат світлини"]}",)
+bl_w = tkinter.Checkbutton(text="Чорно-білий",
+                           variable=bl_w_w,
+                           command = colorist,
+                           width = 20,
+                           height= 2,
+                           font=(os.path.join(PATH, "custom", "modules", "angrybirds-regular3.ttf"), 13),
+                           bg = "turquoise")
+bl_w.pack(padx=6, pady=7, anchor="nw")
+mirr = tkinter.Checkbutton(text="Відзеркалити",
+                           variable=mirr_w,
+                           command = colorist,
+                           width = 20,
+                           height= 2,
+                           font=(os.path.join(PATH, "custom", "modules", "angrybirds-regular3.ttf"), 13),
+                           bg = "turquoise")
+mirr.pack(padx=6, pady=7, anchor="nw")
+blur = tkinter.Checkbutton(text="Розмилення",
+                           variable=blur_w,
+                           command = colorist,
+                           width = 20,
+                           height= 2,
+                           font=(os.path.join(PATH, "custom", "modules", "angrybirds-regular3.ttf"), 13),
+                           bg = "turquoise")
+blur.pack(padx=6, pady=7, anchor="nw")
 
 # Отображение картинки
 try:
-    # imag = info_and_resize_img(os.path.join(PATH, "custom", "modules", "img2.jpg"))
-    # canvas = tkinter.Canvas(menu, height=imag[0]["Висота світлини"][1], width=imag[0]["Ширина світлини"][1], bg = "Black")
-    # image = Image.open(imag[0]["Назва світлини"])
-    # photo = ImageTk.PhotoImage(image)
-    # canvas.create_image(0, 0, anchor=tkinter.NW,image=photo)
-    # canvas.pack(anchor=tkinter.NE, expand=True)
     info_foto(info = info_and_resize_img(os.path.join(PATH, "custom", "modules", "img.jpg")))
 except:
     pass
